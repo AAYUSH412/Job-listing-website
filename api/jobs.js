576522700ddf3af6eb55650b7f0b06f33b3ed527
Aayush <vaghelaaayush412@gmail.com>
2025-01-24 23:37:52 +0530

    Fix Vercel deployment configuration

--- api/jobs.js
+++ api/jobs.js
@@ -1,17 +1,15 @@
-import { join } from 'path';
-
 export const config = {
-  runtime: 'edge'
-};
-
-export default async function handler(req) {
-  const jobsJson = await fetch('https://github.com/AAYUSH412/Job-listing-website/blob/main/src/jobtake.json');
-  const jobs = await jobsJson.json();
-
-  return new Response(JSON.stringify(jobs), {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  });
-}
+    runtime: 'edge'
+  };
+  
+  export default async function handler(req) {
+    const jobsJson = await fetch('https://raw.githubusercontent.com/AAYUSH412/Job-listing-website/main/src/jobtake.json');
+    const jobs = await jobsJson.json();
+  
+    return new Response(JSON.stringify(jobs), {
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  }
